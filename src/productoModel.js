ecfd6833519b5035a0af0b033909264ae70e6569
DiriARG <matidirisio@gmail.com>
2024-07-05 15:39:16 -0300

    Corregimos errores ortográficos

--- src/productoModel.js
+++ src/productoModel.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-// Definir el esquema y el modelo de Mongoose.
+//Definimos el esquema y el modelo de Mongoose.
 const productoSchema = new mongoose.Schema({
   id: Number,
   nombre: String,
   importe: Number,
   categoria: String,
-})
+});
 
-const Producto = mongoose.model('Producto', productoSchema)
+const Producto = mongoose.model("Producto", productoSchema);
 
 //Exportamos el modelo "Producto".
-module.exports = Producto
+module.exports = Producto;
