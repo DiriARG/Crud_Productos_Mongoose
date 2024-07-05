944e5633d722f320a3acfa6f720e4d2bf98adee2
DiriARG <matidirisio@gmail.com>
2024-07-05 17:14:52 -0300

    Desactivamos X-Powered-By, mejoramos cada endpoint y corregimos errores ortográficos.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 //Creamos una instancia de la aplicación Express.
 const app = express();
-//Importamos la función para conectar a la base de datos MongoDB.
+//Importamos la función para conectarse a la base de datos MongoDB.
 const connectDB = require("./src/mongoose.js");
 //Definimos el puerto, predeterminadamente el que se encuentra en el archivo .env, caso contrario el 3008.
 const port = process.env.PORT ?? 3008;
@@ -14,6 +14,9 @@ const Producto = require("./src/productoModel.js");
 //Conectar a MongoDB usando Mongoose.
 connectDB();
 
+//Desactivar el encabezado X-Powered-By por razones de seguridad.
+app.disable("x-powered-by");
+
 //Middleware que sirve para reconocer el objeto de solicitud entrante como un objeto JSON (para POST y PUT).
 app.use(express.json());
 //Middleware que sirve para tener un registro detallado de las solicitudes HTTP en una aplicación Express.js.
@@ -21,15 +24,15 @@ app.use(morgan("dev"));
 
 //Ruta principal (Devuelve un mensaje de bienvenida a la API).
 app.get("/", (req, res) => {
-  res.json("Bienvenido a la API de Productos tecnologicos");
+  res.json("Bienvenido a la API de Productos tecnológicos!🖥️📱");
 });
 
-//Devuelve todos los productos. Permite filtrar por categoría mediante querey string.
+//Devuelve todos los productos. Permite filtrar por categoría mediante query string.
 app.get("/productos", async (req, res) => {
   const { categoria } = req.query;
   let query;
   if (!categoria) {
-    //Si no encuentra categoria en la url...
+    //Si no encuentra categoría en la url...
     query = {}; //Muestra todo...
   } else {
     query = { categoria: { $regex: categoria, $options: "i" } };
@@ -38,7 +41,7 @@ app.get("/productos", async (req, res) => {
     const productos = await Producto.find(query);
     res.json(productos);
   } catch (error) {
-    res.status(500).send("Error al obtener los productos");
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -51,10 +54,15 @@ app.get("/productos/categorias", async (req, res) => {
     const categorias = productos.map((producto) => producto.categoria);
     //Obtenemos las categorías únicas usando un Set.
     const categoriasUnicas = [...new Set(categorias)];
-    res.json(categoriasUnicas);
+    if (!categoriasUnicas) {
+      return res
+        .status(404)
+        .json({ error: "Lista de categorías no encontrada 🕵️❗" });
+    } else {
+      res.json(categoriasUnicas);
+    }
   } catch (error) {
-    console.error("Error al obtener las categorias: ", error);
-    res.status(500).send("Error al obtener las categorias");
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -63,44 +71,62 @@ app.get("/productos/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const producto = await Producto.findById(id);
-    if (producto) {
-      res.json(producto);
+    if (!producto) {
+      return res
+        .status(404)
+        .json({ error: `Producto con ID:${id} no encontrado 🕵️❗ ` });
     } else {
-      res.status(404).json({ message: `Producto con ID:${id} no encontrado` });
+      res.json(producto);
     }
   } catch (error) {
-    res.status(500).json({ message: "Error al buscar un producto por su ID" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
 //Crear un nuevo producto.
 app.post("/productos", async (req, res) => {
+  //Crear una instancia del modelo Producto con los datos recibidos en req.body.
   const nuevoProducto = new Producto(req.body);
   try {
-    await nuevoProducto.save();
-    res.status(201).json(nuevoProducto);
+    //Validamos que todos los campos requeridos estén presentes en req.body.
+    if (
+      !req.body.id ||
+      !req.body.nombre ||
+      !req.body.importe ||
+      !req.body.categoria
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Todos los campos son obligatorios 🚫!" }); //Si no lo estan devolvemos un error 400.
+    } else {
+      //Caso contrario si están todos los campos, guardamos el nuevo producto y devolvemos un mensaje con estatus 201.
+      await nuevoProducto.save();
+      res
+        .status(201)
+        .json({ message: "Nuevo producto creado ✅: ", nuevoProducto });
+    }
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error al crear un nuevo producto" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
 //Eliminar un producto por su ID.
 app.delete("/productos/:id", async (req, res) => {
   const { id } = req.params;
-
   try {
-    const eliminar = await Producto.findByIdAndDelete(id);
-    if (eliminar) {
-      res.json({ message: "Producto eliminado con exito" });
+    const productoEliminado = await Producto.findByIdAndDelete(id);
+    if (!productoEliminado) {
+      return res
+        .status(404)
+        .json({ error: "Producto no encontrado para eliminar 🕵️❗" });
     } else {
-      res.status(404).json({
-        message: "Producto no encontrada para borrar",
+      res.json({
+        message: "Producto eliminado con éxito ✅: ",
+        productoEliminado,
       });
     }
   } catch (error) {
-    return res.status(500).json({ message: "Error al eliminar el producto" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -111,20 +137,18 @@ app.patch("/productos/:id", async (req, res) => {
     const productoActualizado = await Producto.findByIdAndUpdate(id, req.body, {
       new: true, //Devuelve el documento actualizado en lugar del original.
     });
-    if (productoActualizado) {
-      res.json({
-        message: `Producto actualizado parcialmente con exito:`,
-        productoActualizado,
-      });
+    if (!productoActualizado) {
+      return res
+        .status(404)
+        .json({ error: "Producto no encontrado para su actualización 🕵️❗" });
     } else {
-      return res.status(404).json({
-        message: "Producto no encontrada para su actualización parcial",
+      res.json({
+        message: "Producto actualizado con éxito ✅: ",
+        productoActualizado,
       });
     }
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error al actualizar parcialmente un producto" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -134,20 +158,20 @@ app.put("/productos/:id", async (req, res) => {
   try {
     const productoActualizado = await Producto.findByIdAndUpdate(id, req.body, {
       new: true,
-      overwrite: true, //Se agrega overwrite para sobreescribir el documento con los datos proporcionados.
+      overwrite: true, //Se agrega overwrite para sobrescribir el documento con los datos proporcionados.
     });
-    if (productoActualizado) {
-      res.json({
-        message: `Producto actualizado con exito:`,
-        productoActualizado,
-      });
+    if (!productoActualizado) {
+      return res
+        .status(404)
+        .json({ error: "Producto no encontrado para su actualización 🕵️❗" });
     } else {
-      return res.status(404).json({
-        message: "Producto no encontrada para su actualización",
+      res.json({
+        message: "Producto actualizado con éxito ✅: ",
+        productoActualizado,
       });
     }
   } catch (error) {
-    return res.status(500).json({ message: "Error al actualizar un producto" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -159,16 +183,17 @@ app.get("/productos/importes/mayor/:importe", async (req, res) => {
     // Convertir el precio (Array/String) a float por si el usuario digita un número con coma. Siempre lo que ingrese el usuario va a ser un string, por eso se lo parsea en este caso.
     const precioNumerico = parseFloat(importe);
     const productos = await Producto.find({ importe: { $gt: precioNumerico } });
-    if (productos.length > 0) {
-      //Se coloca .lenght mayor a 0 para que compruebe si el array tiene mas de un elemento y pase a ejecutarse la condición else en caso de que no se encuentre un producto con un precio mayor al definido.
-      res.json(productos);
-    } else {
-      res.status(404).json({
-        message: "No se encontro un producto con un precio mayor al indicado ",
+    //Se coloca ".lenght mayor a 0" para que compruebe si el array tiene más de un elemento, en este caso, al colocarle "!" estamos negando esa acción.
+    if (!productos.length > 0) {
+      return res.status(404).json({
+        error:
+          "No se encontró un producto con un precio mayor al indicado 🕵️❗",
       });
+    } else {
+      res.json(productos);
     }
   } catch (error) {
-    res.status(500).json({ message: "Error al obtener los productos" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -179,15 +204,16 @@ app.get("/productos/importes/menor/:importe", async (req, res) => {
   try {
     const precioNumerico = parseFloat(importe);
     const productos = await Producto.find({ importe: { $lt: precioNumerico } });
-    if (productos.length > 0) {
-      res.json(productos);
-    } else {
-      res.status(404).json({
-        message: "No se encontro un producto con un precio menor al indicado ",
+    if (!productos.length > 0) {
+      return res.status(404).json({
+        error:
+          "No se encontró un producto con un precio menor al indicado 🕵️❗",
       });
+    } else {
+      res.json(productos);
     }
   } catch (error) {
-    res.status(500).json({ message: "Error al obtener los productos" });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -199,17 +225,17 @@ app.get("/productos/nombre/:nombre", async (req, res) => {
     const productos = await Producto.find({
       nombre: { $regex: nombre, $options: "i" }, //Crea una expresión regular con el nombre del producto y el modificador "i" para que la búsqueda sea insensible a mayúsculas y minúsculas.
     });
-    if (productos) {
-      res.json(productos);
+    //Si no se encuentran los productos...
+    if (!productos) {
+      return res.status(404).json({
+        error: "No se encontraron los productos especificados 🕵️❗",
+      });
     } else {
-      res
-        .status(404)
-        .json({ message: "No se encontraron los productos especificados" });
+      //Caso contrario se muestran...
+      res.json(productos);
     }
   } catch (error) {
-    res.status(500).json({
-      message: "Error al obtener los productos con el nombre especificado",
-    });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
@@ -217,24 +243,29 @@ app.get("/productos/nombre/:nombre", async (req, res) => {
 app.get("/productos/rango/:min/:max", async (req, res) => {
   const { min, max } = req.params;
   try {
+    //Convertimos min y max a números, ya que req.params devuelve strings.
+    const minNum = parseFloat(min);
+    const maxNum = parseFloat(max);
+
+    //En la consulta pedimos que sea greather than or equal to $gte (mayor o igual a) y less than or equal to $lte (menor a o igual a) para poder tener un rango de precios.
     const rangoEspecificado = await Producto.find({
-      importe: { $gte: min, $lte: max },
+      importe: { $gte: minNum, $lte: maxNum },
     });
-    if (rangoEspecificado) {
-      res.json(rangoEspecificado);
-    } else {
-      res.status(404).json({
-        message: "No se encontraron productos dentro del rango especificado",
+
+    //Verificar si no se encontraron productos dentro del rango.
+    if (rangoEspecificado.length === 0) {
+      return res.status(404).json({
+        error: "No se encontraron productos dentro del rango especificado 🕵️❗",
       });
+    } else {
+      res.json(rangoEspecificado);
     }
   } catch (error) {
-    res.status(500).json({
-      message: "Error al obtener los productos dentro del rango especificado",
-    });
+    res.status(500).json({ error: "Error del servidor 🚫⚙️" });
   }
 });
 
-//Inicializamos el servidor
+//Inicializamos el servidor.
 app.listen(port, () => {
   console.log(`Servidor escuchando en: http://localhost:${port}`);
 });
